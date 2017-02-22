6a7260d9aba618718515ff3acfb08a7cecf7372e
Teodor Todorov <banskaliqtabg@gmail.com>
2017-02-22 17:20:21 +0200

    delete no cascade orphan

--- user-management-spring/src/main/java/com/example/associations/nocascadedeleteorphan/StockService5.java
+++ user-management-spring/src/main/java/com/example/associations/nocascadedeleteorphan/StockService5.java
@@ -15,34 +15,34 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class StockService5 {
-
+    
     @Autowired
     private StockRepository5 repo;
-
+    
     @Autowired
     private StockDailyRecordRepository5 stockDailyRecorderRepo;
-
+    
     public List<Stock5> getAllStocks() {
         return repo.findAll();
     }
-
+    
     List<StockDailyRecord5> getAllRecords() {
         return stockDailyRecorderRepo.findAll();
     }
-
+    
     Stock5 deleteStock(Long id) {
         Stock5 tempStock = repo.findOne(id);
-
+        
         if (tempStock == null) {
             return null;
         }
 
-        stockDailyRecorderRepo.removeByStockStockId(id);
+        //stockDailyRecorderRepo.removeByStockStockId(id);
+        repo.delete(id);
 
         //tempStock.setRecords(null);
-        repo.save(tempStock);
-
+        //repo.save(tempStock);
         return tempStock;
     }
-
+    
 }
