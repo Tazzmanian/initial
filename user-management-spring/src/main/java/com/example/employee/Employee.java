bb969abb14f40a4f3d235187c3d164b6a0cfe487
Teodor Todorov <banskaliqtabg@gmail.com>
2017-02-24 15:33:09 +0200

    fix save employee response

--- user-management-spring/src/main/java/com/example/employee/Employee.java
+++ user-management-spring/src/main/java/com/example/employee/Employee.java
@@ -57,7 +57,7 @@ public class Employee implements Serializable {
     private BigDecimal bonus;
     private BigDecimal commission;
 
-    @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "USER_ID")
     private User user;
 
